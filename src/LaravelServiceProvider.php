a99034c8ba95eabad062bab6eb3f2b673f909223
yame <14428812@qq.com>
2018-12-13 18:51:51 +0800

    init

---
+++ src/LaravelServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Yame\Banner;
+
+use Illuminate\Support\ServiceProvider as Provider;
+
+class LaravelServiceProvider extends Provider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
